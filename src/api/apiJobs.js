1c48aed7fbeb620cc2e941197ea2e71f861ba7a5
Manmit Pal <manmitpal348@gmail.com>
2024-12-30 21:57:09 +0530

    Added filter to filter out companies and jobs

--- src/api/apiJobs.js
+++ src/api/apiJobs.js
@@ -29,4 +29,38 @@ export async function getJobs(token, {location, company_id, searchQuery}){
     return data;
 
 }
-// user_2qspAI1F36FBkwlBxNM98bgZ9Jn <= client ID
+
+export async function saveJob(token, {alreadySaved}, saveData){
+    const supabase = await supabaseClient(token);
+    
+    if(alreadySaved){
+    const {data, error:deleteError} = await supabase
+    .from("saved_jobs")
+    .delete()
+    .eq("job_id", saveData.job_id)
+
+     if(deleteError){
+        
+        console.error("Error deleting saved jobs", deleteError);
+        return null;
+     }
+
+     return data;
+
+    }else{
+
+       const {data, error:insertError} = await supabase
+       .from("saved_jobs")
+       .insert([saveData])
+       .select();
+
+     if(insertError){
+        console.error("Error  saved jobs", insertError);
+        return null;
+     }
+     return data;
+    }
+
+}
+// us
+// er_2qspAI1F36FBkwlBxNM98bgZ9Jn <= client ID
--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,6 +1,12 @@
 import { useUser } from "@clerk/clerk-react"
-import { Card, CardHeader, CardTitle } from "./ui/card"
-import { Trash2Icon } from "lucide-react"
+import { Link } from "react-router-dom"
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "./ui/card"
+import { Heart, MapPinIcon, Trash2Icon } from "lucide-react"
+import { Button } from "./ui/button"
+import useFetch from "@/hooks/use-fetch"
+import { saveJob } from "@/api/apiJobs"
+import SavedJobs from "@/pages/SavedJobs"
+import { useEffect, useState } from "react"
 
 const JobCard = ({
     job,
@@ -9,7 +15,25 @@ const JobCard = ({
     onJobSaved =() => {},
 }) => {
 
+  const [saved, setSaved] = useState(saveInit);
+
+  const {fn:fnSavedJob, data:savedJob, loading:loadingSavedJob} = useFetch(saveJob, {
+    alreadySaved: saved,
+  });
+
   const {user} = useUser()
+
+  const handleSaveJob = async  () => {
+    await fnSavedJob({
+      user_id: user.id,
+      job_id: job.id
+    });
+    onJobSaved();
+  }
+
+  useEffect(() => {
+    if(savedJob !== undefined) setSaved(savedJob?.length > 0)
+  },[savedJob])
 
   return <>
    <Card>
@@ -22,6 +46,31 @@ const JobCard = ({
             </CardTitle>
         
     </CardHeader>
+    <CardContent className="flex flex-col gap-4 flex-1" >
+           <div className="flex justify-between" >
+            {job.company && <img src={job.company.logo_url} className="h-6"/>}
+           <div className="flex gap-2 items-center" >
+              <MapPinIcon size={15} />{job.location}
+          </div>
+        </div>
+        <hr/>
+        {job.description.substring(0, job.description.indexOf("."))}
+    </CardContent>
+    <CardFooter className="flex gap-2">
+        <Link to={`/job/${job.id}`} className="flex-1" >
+          <Button variant='secondary' className="w-full" >More Details</Button>
+         </Link>
+
+         {!isMyJob && (
+          <Button variant="outline" className="w-15" onClick={handleSaveJob} disabled={loadingSavedJob} >
+             {saved ? (
+               <Heart size={20} stroke="red" fill="red" />
+              ) : (
+              <Heart size={20} />
+            ) }
+          </Button>
+         )}
+    </CardFooter>
    </Card>
   </>
 }
